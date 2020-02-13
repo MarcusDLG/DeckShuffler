79c648841bf7755617419114eb9f96d037da4569
git config --global hub.protocol https <>
2020-02-13 10:32:32 -0500

    corrected lists of ranks and suits

--- Program.cs
+++ Program.cs
@@ -19,61 +19,70 @@ namespace DeckShuffler
 
         // build an array of each number and each card type or 4 separate arrays that you reference and interpolate to shuffle
 
-        var deck = new List<string> {
-          "Ace of Spades",
-          "2 of Spades",
-          "3 of Spades",
-          "4 of Spades",
-          "5 of Spades",
-          "6 of Spades",
-          "7 of Spades",
-          "8 of Spades",
-          "9 of Spades",
-          "10 of Spades",
-          "Jack of Spades",
-          "Queen of Spades",
-          "King of Spades",
-          "Ace of Hearts",
-          "2 of Hearts",
-          "3 of Hearts",
-          "4 of Hearts",
-          "5 of Hearts",
-          "6 of Hearts",
-          "7 of Hearts",
-          "8 of Hearts",
-          "9 of Hearts",
-          "10 of Hearts",
-          "Jack of Hearts",
-          "Queen of Hearts",
-          "King of Hearts",
-          "Ace of Diamonds",
-          "2 of Diamonds",
-          "3 of Diamonds",
-          "4 of Diamonds",
-          "5 of Diamonds",
-          "6 of Diamonds",
-          "7 of Diamonds",
-          "8 of Diamonds",
-          "9 of Diamonds",
-          "10 of Diamonds",
-          "Jack of Diamonds",
-          "Queen of Diamonds",
-          "King of Diamonds",
-          "Ace of Clubs",
-          "2 of Clubs",
-          "3 of Clubs",
-          "4 of Clubs",
-          "5 of Clubs",
-          "6 of Clubs",
-          "7 of Clubs",
-          "8 of Clubs",
-          "9 of Clubs",
-          "10 of Clubs",
-          "Jack of Clubs",
-          "Queen of Clubs",
-          "King of Clubs",
-        };
-
+        // var deck = new List<string> {
+        //   "Ace of Spades",
+        //   "2 of Spades",
+        //   "3 of Spades",
+        //   "4 of Spades",
+        //   "5 of Spades",
+        //   "6 of Spades",
+        //   "7 of Spades",
+        //   "8 of Spades",
+        //   "9 of Spades",
+        //   "10 of Spades",
+        //   "Jack of Spades",
+        //   "Queen of Spades",
+        //   "King of Spades",
+        //   "Ace of Hearts",
+        //   "2 of Hearts",
+        //   "3 of Hearts",
+        //   "4 of Hearts",
+        //   "5 of Hearts",
+        //   "6 of Hearts",
+        //   "7 of Hearts",
+        //   "8 of Hearts",
+        //   "9 of Hearts",
+        //   "10 of Hearts",
+        //   "Jack of Hearts",
+        //   "Queen of Hearts",
+        //   "King of Hearts",
+        //   "Ace of Diamonds",
+        //   "2 of Diamonds",
+        //   "3 of Diamonds",
+        //   "4 of Diamonds",
+        //   "5 of Diamonds",
+        //   "6 of Diamonds",
+        //   "7 of Diamonds",
+        //   "8 of Diamonds",
+        //   "9 of Diamonds",
+        //   "10 of Diamonds",
+        //   "Jack of Diamonds",
+        //   "Queen of Diamonds",
+        //   "King of Diamonds",
+        //   "Ace of Clubs",
+        //   "2 of Clubs",
+        //   "3 of Clubs",
+        //   "4 of Clubs",
+        //   "5 of Clubs",
+        //   "6 of Clubs",
+        //   "7 of Clubs",
+        //   "8 of Clubs",
+        //   "9 of Clubs",
+        //   "10 of Clubs",
+        //   "Jack of Clubs",
+        //   "Queen of Clubs",
+        //   "King of Clubs",
+        // };
+        var suits = new List<string>() { "clubs", "spades", "hearts", "diamonds" };
+        var ranks = new List<string>() { "ace", "two", "three", "four", "five", "six", "7", "8", "9", "10", "Jack", "Queen", "King" };
+        var deck = new List<string>();
+        for (int i = 0; i < suits.Count; i++)
+        {
+          for (int j = 0; j < ranks.Count; j++)
+          {
+            deck.Add($"{ranks[j]} of {suits[i]}");
+          }
+        }
         // Console.WriteLine(deck[10]);
         // After deck creation, you should shuffle the deck using Fisher–Yates shuffle algorithm.
 
@@ -93,7 +102,7 @@ namespace DeckShuffler
         };
         // After the deck is shuffled, display the top card.
         var cardPlayed = deck[0];
-        Console.WriteLine(cardPlayed);
+        Console.WriteLine($"the first card played is: {cardPlayed}");
         deck.Remove(cardPlayed);
         playerHand.Add(cardPlayed);
 
@@ -117,7 +126,7 @@ namespace DeckShuffler
           if (input == "next")
           {
             cardPlayed = deck[0];
-            Console.WriteLine(cardPlayed);
+            // Console.WriteLine(cardPlayed);
             Console.WriteLine($"The next card on top is: {deck[0]}");
             deck.Remove(cardPlayed);
             if (deck.Count <= 1)
